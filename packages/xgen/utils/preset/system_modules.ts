b23ed6e06eafcb9a6481cead44a9655bc38dc26b
wwsheng <wwsheng@qq.com>
2023-02-03 22:09:35 +0800

    更新远程组件依赖

--- packages/xgen/utils/preset/system_modules.ts
+++ packages/xgen/utils/preset/system_modules.ts
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import ReactDomClient from 'react-dom/client'
 import JsxRuntime from 'react/jsx-runtime'
-import  * as antd from 'antd'//为了方便导入所有的控件
+import Antd from 'antd'//为了方便导入所有的控件
+import Ahooks from 'ahooks'
 
 System.set('app:react', { default: React, __useDefault: true })
 System.set('app:react/jsx-runtime', { ...JsxRuntime })
 System.set('app:react-dom', { default: ReactDom, __useDefault: true })
 System.set('app:react-dom/client', { default: ReactDomClient, __useDefault: true })
 
-System.set('app:antd',{...antd})//注册所有的antd控件
+System.set('app:antd',{...Antd})//注册所有的antd控件
+System.set('app:ahooks',{...Ahooks})
 
 System.addImportMap({
 	imports: {
@@ -17,6 +19,7 @@ System.addImportMap({
 		'react/jsx-runtime': 'app:react/jsx-runtime',
 		'react-dom': 'app:react-dom',
 		'react-dom/client': 'app:react-dom/client',
-		'antd':'app:antd'//增加antd外部依赖的映射,只有这样，生成的远程控件才能找到对应的控件
+		'antd':'app:antd',//增加antd外部依赖的映射,只有这样，生成的远程控件才能找到对应的控件
+		'ahooks':'app:ahooks'
 	}
 })
